ad90e5e953915b69e26918db6475372c76d4c861
david robison <mrdavidrobison@gmail.com>
2017-06-27 19:29:15 -0600

    add click function, show and hide stuff

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,17 @@
 $(document).ready(function(){
-  $("#grid").hide();
+
+  var count = 0;
   
+  $("#grid").hide();
+  $("#playGame").show();
+  
+  $(".btn-success").click(function(){
+    $("#grid").show();
+    $("#playGame").hide();
+  })
+
+  $("#1, #2, #3, #4, #5, #6, #7, #8, #9").click(function(){
+    
+  })
+
 })
