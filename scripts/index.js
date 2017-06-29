5179d07b40a4dfd90a68fd5c9a9576aebdc965be
david robison <mrdavidrobison@gmail.com>
2017-06-29 08:19:48 -0600

    hide and show elements

--- scripts/index.js
+++ scripts/index.js
@@ -2,16 +2,22 @@ $(document).ready(function(){
 
   var count = 0;
   
-  $("#grid").hide();
   $("#playGame").show();
+  $("#grid").hide();
+  $("#player1").hide();
+  $("#player2").hide();
   
   $(".btn-success").click(function(){
-    $("#grid").show();
+    $("#player1").show();
     $("#playGame").hide();
   })
 
+  $("#x").click(function(){
+
+  })
+
   $("#1, #2, #3, #4, #5, #6, #7, #8, #9").click(function(){
-    
+
   })
 
 })
