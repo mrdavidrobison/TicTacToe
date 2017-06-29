94bc58524bc827a5b902db177d78ca6338c0d158
david robison <mrdavidrobison@gmail.com>
2017-06-29 08:39:35 -0600

    get rid of player2 stepper, at x, o assignments if-else

--- scripts/index.js
+++ scripts/index.js
@@ -1,13 +1,13 @@
 $(document).ready(function(){
 
-  var player;
+  var player1;
+  var player2;
   var totalCount = 0;
   var player1Count = 0;
   var player2Count = 0;
   
   $("#playGame").show();
   $("#player1").hide();
-  $("#player2").hide();
   $("#grid").hide();
   
   $("#begin").click(function(){
@@ -16,18 +16,15 @@ $(document).ready(function(){
   })
 
   $("#1x, #1o").click(function(){
-    $("#player2").show();
-    $("#player1").hide();
-  })
-
-  $("#2x, #2o").click(function(){
     $("#grid").show();
-    $("#player2").hide();
-  })
-
-  $("#begin").click(function(){
-    $("#player1").show();
-    $("#playGame").hide();
+    $("#player1").hide();
+    if ($("#1x").click() === true){
+      player1 = "X";
+      player2 = "O";
+    } else {
+      player1 = "O";
+      player2 = "X";
+    }
   })
 
   $("#begin").click(function(){
