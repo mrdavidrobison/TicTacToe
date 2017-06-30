f16490d1633862c7e45fe5f2cf83fe137ac8dc38
david robison <mrdavidrobison@gmail.com>
2017-06-30 16:11:49 -0600

    insert click functions for each square, need to find better solution

--- scripts/index.js
+++ scripts/index.js
@@ -3,8 +3,8 @@ $(document).ready(function(){
   var player1;
   var player2;
   var totalCount = 0;
-  var turn = player1;
-  
+  var turn=true;
+
   // declare the first page 
   $("#grid").hide();
   $("#playGame").show();
@@ -28,16 +28,131 @@ $(document).ready(function(){
     player1 = "X";
     player2 = "O";
   })
-
   $("#1o").click(function(){
     player1 = "O";
     player2 = "X";
   })
 
-  // change squares to X's and O's
+  // determine number of clicks allowable and switch turns
   $("#1").click(function(){
+    totalCount++;
+    if (turn === true){
       $("#1").html(player1);
-  
+      $("h2").html("Player 2 go!");
+      turn=false;
+    } else {
+      $("#1").html(player2);
+      $("h2").html("Player 1 go!");
+      turn=true;
+    }    
   })
 
+  $("#2").click(function(){
+    totalCount++;
+    if (turn === true){
+      $("#2").html(player1);
+      $("h2").html("Player 2 go!");
+      turn=false;
+    } else {
+      $("#2").html(player2);
+      $("h2").html("Player 1 go!");
+      turn=true;
+    }    
+  })
+
+  $("#3").click(function(){
+    totalCount++;
+    if (turn === true){
+      turn=false;
+      $("#3").html(player1);
+      $("h2").html("Player 2 go!");
+    } else {
+      turn=true;
+      $("#3").html(player2);
+      $("h2").html("Player 1 go!");
+    }    
+  })
+
+  $("#4").click(function(){
+    totalCount++;
+    if (turn === true){
+      turn=false;
+      $("#4").html(player1);
+      $("h2").html("Player 2 go!");
+    } else {
+      turn=true;
+      $("#4").html(player2);
+      $("h2").html("Player 1 go!");
+    }    
+  })
+
+  $("#5").click(function(){
+    totalCount++;
+    if (turn === true){
+      turn=false;
+      $("#5").html(player1);
+      $("h2").html("Player 2 go!");
+    } else {
+      turn=true;
+      $("#5").html(player2);
+      $("h2").html("Player 1 go!");
+    }    
+  })
+
+  $("#6").click(function(){
+    totalCount++;
+    if (turn === true){
+      turn=false;
+      $("#6").html(player1);
+      $("h2").html("Player 2 go!");
+    } else {
+      turn=true;
+      $("#6").html(player2);
+      $("h2").html("Player 1 go!");
+    }    
+  })
+
+  $("#7").click(function(){
+    totalCount++;
+    if (turn === true){
+      turn=false;
+      $("#7").html(player1);
+      $("h2").html("Player 2 go!");
+    } else {
+      turn=true;
+      $("#7").html(player2);
+      $("h2").html("Player 1 go!");
+    }    
+  })
+
+  $("#8").click(function(){
+    totalCount++;
+    if (turn === true){
+      turn=false;
+      $("#8").html(player1);
+      $("h2").html("Player 2 go!");
+    } else {
+      turn=true;
+      $("#8").html(player2);
+      $("h2").html("Player 1 go!");
+    }    
+  })
+
+  $("#9").click(function(){
+    totalCount++;
+    if (turn === true){
+      turn=false;
+      $("#9").html(player1);
+      $("h2").html("Player 2 go!");
+    } else {
+      turn=true;
+      $("#9").html(player2);
+      $("h2").html("Player 1 go!");
+    }    
+  })
+
+  // game over after 9 clicks
+  if (totalCount = 9){
+    $("h2").html("GAME OVER!");
+  } 
 })
