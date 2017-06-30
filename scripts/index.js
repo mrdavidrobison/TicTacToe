c465348497c29b7b41f153284b524e8dd02e3d6a
david robison <mrdavidrobison@gmail.com>
2017-06-30 11:51:43 -0600

    delete a var, perfect stepper pages

--- scripts/index.js
+++ scripts/index.js
@@ -3,41 +3,41 @@ $(document).ready(function(){
   var player1;
   var player2;
   var totalCount = 0;
-  var player1Count = 0;
-  var player2Count = 0;
+  var turn = player1;
   
+  // declare the first page 
+  $("#grid").hide();
   $("#playGame").show();
   $("#player1").hide();
-  $("#grid").hide();
+  $("#X").hide();
+  $("#O").hide();
   
+  // declare the second page after "begin" button is clicked
   $("#begin").click(function(){
     $("#player1").show();
     $("#playGame").hide();
+    // declare the third page after either "X's" or "O's" buttons are clicked
+    $("#1x, #1o").click(function(){
+      $("#grid").show();
+      $("#player1").hide();
+    })
   })
 
-  $("#1x, #1o").click(function(){
-    $("#grid").show();
-    $("#player1").hide();
-    if ($("#1x").click() === true){
-      player1 = "X";
-      player2 = "O";
-    } else {
-      player1 = "O";
-      player2 = "X";
-    }
+  // assign X's and O's to player 1 and 2
+  $("#1x").click(function(){
+    player1 = "X";
+    player2 = "O";
   })
 
-  $("#begin").click(function(){
-    $("#player1").show();
-    $("#playGame").hide();
+  $("#1o").click(function(){
+    player1 = "O";
+    player2 = "X";
   })
 
-  $("#x").click(function(){
-
-  })
-
-  $("#1, #2, #3, #4, #5, #6, #7, #8, #9").click(function(){
-
+  // change squares to X's and O's
+  $("#1").click(function(){
+      $("#1").html(player1);
+  
   })
 
 })
